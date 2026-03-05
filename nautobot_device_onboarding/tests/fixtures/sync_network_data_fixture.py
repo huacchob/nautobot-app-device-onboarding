b155aeb49750b8201b27df138ca1e7f9f1e35bc6
Carlos Huaccho <huaccho.carlos0612@gmail.com>
2026-03-05 14:08:04 -0500

    adding power supplies

--- nautobot_device_onboarding/tests/fixtures/sync_network_data_fixture.py
+++ nautobot_device_onboarding/tests/fixtures/sync_network_data_fixture.py
@@ -118,6 +118,14 @@ sync_network_mock_data_valid = {
                 "description": "4x1G Uplink Module",
             },
         },
+        "power_supplies": {
+            "Switch 1 Power Supply A": {
+                "module_type": "PWR-C1-350WAC",
+                "manufacturer": "Cisco",
+                "serial": "ART2150F0BH",
+                "description": "350W AC Power Supply",
+            },
+        },
     },
     "demo-cisco-2": {
         "serial": "9ABUXU5882222",
