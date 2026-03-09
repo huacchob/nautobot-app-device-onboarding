2f23cf8d49190407214d9d06d5e46612095ef215
Carlos Huaccho <carlos.huaccho@networktocode.com>
2026-03-09 09:45:53 -0400

    adding validation check

--- nautobot_device_onboarding/diffsync/adapters/sync_network_data_adapters.py
+++ nautobot_device_onboarding/diffsync/adapters/sync_network_data_adapters.py
@@ -385,7 +385,10 @@ class SyncNetworkDataNautobotAdapter(FilteredNautobotAdapter):
     def load_module_bay(self):
         """Load Module Bays into the Diffsync store."""
         for module_bay in ModuleBay.objects.all():
-            if not module_bay.parent_device.name:
+            if not module_bay.name or not module_bay.parent_device or not module_bay.parent_device.name:
+                self.job.logger.warning(
+                    f"Module Bay {module_bay} is missing a name or parent device with a name. Skipping load for this Module Bay."
+                )
                 continue
             network_module_bay = self.module_bay(
                 adapter=self,
@@ -529,9 +532,7 @@ class SyncNetworkDataNautobotAdapter(FilteredNautobotAdapter):
                         if interface_name:
                             try:
                                 interface = device.all_interfaces.get(name=interface_name)
-                                IPAddressToInterface.objects.get_or_create(
-                                    interface=interface, ip_address=ip_address
-                                )
+                                IPAddressToInterface.objects.get_or_create(interface=interface, ip_address=ip_address)
                                 self.job.logger.info(
                                     f"Re-assigned {ip_address} to interface {interface_name} on device {device.name}"
                                 )
